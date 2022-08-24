84bf5b098c9f9fad565bb44009ebeab4d1dd014c
Nikolaus Matten <nfmatten@gmail.com>
2022-08-24 12:13:50 -0600

    added second object, alarm_two

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ from alarm_clock import AlarmClock
 alarm_one = AlarmClock()
 alarm_one.set_current_time("12:00")
 alarm_one.set_alarm_time("12:30")
-alarm_one.toggle_alarm("On")
+alarm_one.toggle_alarm("on")
 print(alarm_one.display_alarm_clock())
+
+alarm_two = AlarmClock()
+alarm_two.set_current_time("1:48 pm")
+alarm_two.set_alarm_time("3:00 am")
+alarm_two.toggle_alarm("off")
+print(alarm_two.display_alarm_clock())
